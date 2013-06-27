bc4e5c45987d33f8cecd1a36165fc48562aa6747
pizzli <pizzli@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-27 14:34:52 +0000

    Version 1.1 - Fixed autorotate issue
    git-svn-id: https://plugins.svn.wordpress.org/featured-youtubeimage-slider/trunk@732866 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ytimagerotator.php
+++ ytimagerotator.php
@@ -3,7 +3,7 @@
 Plugin Name: Featured YouTube/Image Slider
 Plugin URI: http://wordpress.org/plugins/featured-youtubeimage-slider/
 Description: A YouTube/Image Rotator for WordPress
-Version: 1.0
+Version: 1.1
 Author: Alexander C. Block
 Author URI: http://pizzli.com
 License: GPLv2
@@ -25,12 +25,10 @@ License: GPLv2
 */
 
 function AddScripts(){
-wp_register_script('jqueryuitabsrotatecustom', plugins_url('/js/jquery-ui-tabs-rotate.js',__FILE__));
 wp_register_style('ytirstylesheet', plugins_url('/css/style.css',__FILE__));
 wp_enqueue_script('jquery');
 wp_enqueue_script('jquery-ui-core');
 wp_enqueue_script('jquery-ui-tabs');
-wp_enqueue_script('jqueryuitabsrotatecustom');
 wp_enqueue_style('ytirstylesheet');
 }
 add_action( 'wp_enqueue_scripts', 'AddScripts' );
@@ -83,6 +81,84 @@ function OutputSlider(){
 echo '
 <script>
 var j = jQuery.noConflict();
+j(document).ready(function(){  
+;(function(j){
+	j.extend( j.ui.tabs.prototype, {
+		rotation: null,
+		rotationDelay: null,
+		continuing: null,
+		rotate: function( ms, continuing ) {
+			var self = this,
+				o = this.options;
+
+			if((ms > 1 || self.rotationDelay === null) && ms !== undefined){//only set rotationDelay if this is the first time through or if not immediately moving on from an unpause
+				self.rotationDelay = ms;
+			}
+
+			if(continuing !== undefined){
+				self.continuing = continuing;
+			}
+
+			var rotate = self._rotate || ( self._rotate = function( e ) {
+				clearTimeout( self.rotation );
+				self.rotation = setTimeout(function() {
+					var t = o.selected;
+					self.select( ++t < self.anchors.length ? t : 0 );
+				}, ms );
+
+				if ( e ) {
+					e.stopPropagation();
+				}
+			});
+
+			var stop = self._unrotate || ( self._unrotate = !continuing
+				? function(e) {
+					if (e.clientX) { // in case of a true click
+						self.rotate(null);
+					}
+				}
+				: function( e ) {
+					t = o.selected;
+					rotate();
+				});
+
+			// start rotation
+			if ( ms ) {
+				this.element.bind( "tabsshow", rotate );
+				this.anchors.bind( o.event + ".tabs", stop );
+				rotate();
+			// stop rotation
+			} else {
+				clearTimeout( self.rotation );
+				this.element.unbind( "tabsshow", rotate );
+				this.anchors.unbind( o.event + ".tabs", stop );
+				delete this._rotate;
+				delete this._unrotate;
+			}
+
+			//rotate immediately and then have normal rotation delay
+			if(ms === 1){
+				//set ms back to what it was originally set to
+				ms = self.rotationDelay;
+			}
+
+			return this;
+		},
+		pause: function() {
+			var self = this,
+				o = this.options;
+
+			self.rotate(0);
+		},
+		unpause: function(){
+			var self = this,
+				o = this.options;
+
+			self.rotate(1, self.continuing);
+		}
+	});
+})(jQuery);
+});
 j(document).ready(function(){  
 j("#featured").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 5000, true);  
 j("#featured").hover(  
